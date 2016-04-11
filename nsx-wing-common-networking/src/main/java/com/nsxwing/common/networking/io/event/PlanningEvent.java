4e94cba4030fb718c2842ed798b804c968a7338c
Chris Greff <CGreff@Pillartechnology.com>
2016-04-11 12:56:41 -0400

    Making the planning event a bit nicer to use.

--- nsx-wing-common-networking/src/main/java/com/nsxwing/common/networking/io/event/PlanningEvent.java
+++ nsx-wing-common-networking/src/main/java/com/nsxwing/common/networking/io/event/PlanningEvent.java
@@ -1,12 +1,14 @@
 package com.nsxwing.common.networking.io.event;
 
 import com.nsxwing.common.state.GameState;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PlanningEvent implements GameplayEvent {
 	private GameState gameState;
 }
